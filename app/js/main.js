166a3a425fecc46e6edb6b9ce775e9a53a3670cc
mikas813 <mishalas@icloud.com>
2020-04-19 21:11:50 +0100

    added flags for every country and changed background

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,6 @@
 const totalInfoElement = document.querySelector('.side-bar-info');
 const infoByCountryElement = document.querySelector('.main-section-info');
 const selectForCountries = document.querySelector('.select-country');
-const sectionForColorado = document.querySelector('.colorado-info');
-
 
 fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
     "method": "GET",
@@ -23,8 +21,6 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
             block.innerHTML += `${itemNameToUpperCase.split('_').join(' ')}: <span>${deathsInfo[item]}`;
         }
     });
-
-
 fetch("https://covid19-data.p.rapidapi.com/geojson-us", {
     "method": "GET",
     "headers": {
@@ -32,25 +28,26 @@ fetch("https://covid19-data.p.rapidapi.com/geojson-us", {
         "x-rapidapi-key": "6d291bb001msh74f879e7cae0d1ep1a2a38jsnfdb0b0ba682d"
     }
 })
-
     .then(function (casesByState) {
         return casesByState.json()
     })
     .then(function (usaDeathsInfo) {
+        let stateName = document.createElement('div');
+        let usaInner = document.querySelector('.usa')
         for (let key in usaDeathsInfo) {
-
             for (let item in usaDeathsInfo[key]) {
                 for (let k in usaDeathsInfo[key][item]) {
                     if (usaDeathsInfo[key][item][k]['name'] === 'Colorado') {
+                        stateName.classList.add(usaDeathsInfo[key][item][k]['name'].toLowerCase());
                         let infoInner = document.createElement('p');
                         infoInner.innerHTML += `In <span class="state">${usaDeathsInfo[key][item][k]['name']}</span> confirmed: <span class="cases">${usaDeathsInfo[key][item][k]['confirmed']}</span> deaths: <span class="cases">${usaDeathsInfo[key][item][k]['deaths']}</span>`
-                        sectionForColorado.appendChild(infoInner);
+                        stateName.appendChild(infoInner);
+                        usaInner.appendChild(stateName);
                     }
                 }
             }
         }
     });
-
 
 fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
     "method": "GET",
@@ -64,33 +61,49 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.p
             return response.json()
         })
     .then(function (info) {
+        // console.log(info)
+
+
         let countriesForSelect = [];
-        let c = [];
         for (let key in info) {
             for (let i = 0; i < info[key].length; i++) {
                 let contentInner = document.createElement('article');
+                let flagInner = document.createElement('img');
                 contentInner.style.marginBottom = "40px";
                 infoByCountryElement.appendChild(contentInner);
                 let classNameByCountry = info[key][i]['country_name'];
                 if (classNameByCountry === '') {
                     classNameByCountry = 'empty';
                 }
+
                 if (typeof classNameByCountry === 'string' && classNameByCountry !== '') {
+                    contentInner.setAttribute('data-country-name', classNameByCountry);
                     countriesForSelect += info[key][i]['country_name'] + '*';
                     classNameByCountry = classNameByCountry.toLowerCase();
                     classNameByCountry = classNameByCountry.split(' ').join('_');
                     contentInner.classList.add(classNameByCountry, 'country_inner');
                     contentInner.setAttribute("id", classNameByCountry);
+                    if (contentInner.classList.contains(classNameByCountry)) {
+                        flagInner.classList.add('country-flag', classNameByCountry);
+                            //
+                            flagInner.setAttribute('src', `../flags/${classNameByCountry}.png`);
+                            contentInner.appendChild(flagInner);
+                    }
                 }
+                console.log(classNameByCountry)
+
+
                 for (let item in info[key][i]) {
                     let countryInfo = document.createElement('p');
                     contentInner.appendChild(countryInfo);
                     countryInfo.classList.add(item, 'country_info');
                     let itemNameToUpperCase = item[0].toUpperCase() + item.slice(1);
                     countryInfo.innerHTML += `${itemNameToUpperCase.split('_').join(' ')}: <span>${info[key][i][item]}`;
+
                 }
             }
         }
+
         countriesForSelect = countriesForSelect.split('*').sort();
         for (let key in countriesForSelect) {
             let anchorForCountry = document.createElement('a');
@@ -99,15 +112,12 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.p
             anchorForCountry.setAttribute("href", "#" + countriesForSelect[key].toLowerCase());
             anchorForCountry.classList.add('search-country');
         }
-
     });
-
 
 document.querySelector('.hide-window').onclick = () => document.querySelector('.select-country').classList.toggle('full-height');
 
 let countrySearchInput = document.getElementById('search-input');
 let allCountries = document.getElementsByClassName('search-country');
-
 
 function hideAllCounties() {
     for (let i = 0; i < allCountries.length; i++) {
@@ -120,8 +130,8 @@ function hideAllCounties() {
         }
     }
 }
-
 countrySearchInput.onkeyup = hideAllCounties;
+
 var btn = $('#button');
 
 $(window).scroll(function() {
@@ -131,9 +141,9 @@ $(window).scroll(function() {
         btn.removeClass('show');
     }
 });
-
 btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({scrollTop:0}, '300');
 });
+
 
